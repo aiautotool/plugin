98a453d1b878c8e0252ef0caf3b57d809b09ef44
aiautotool <161418348+aiautotool@users.noreply.github.com>
2024-02-27 12:54:56 +0700

    font
    add font

---
+++ css/font.js
@@ -0,0 +1,7 @@
+
+var linkElement = document.createElement('link');
+
+linkElement.rel = 'stylesheet';
+linkElement.href = 'https://raw.githubusercontent.com/aiautotool/plugin/main/css/font/css/all.css';
+
+document.head.appendChild(linkElement);
